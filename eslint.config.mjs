d1cd3dc690a66cff48015077b11f68665e733aa7
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-11-14 16:12:47 -0300

    chore: setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,46 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import prettier from 'eslint-plugin-prettier'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'jsx-a11y/alt-text': [
+        'warn',
+        {
+          elements: ['img'],
+          img: ['Image'],
+        },
+      ],
+      'jsx-a11y/aria-props': 'warn',
+      'jsx-a11y/aria-proptypes': 'warn',
+      'jsx-a11y/aria-unsupported-elements': 'warn',
+      'jsx-a11y/role-has-required-aria-props': 'warn',
+      'jsx-a11y/role-supports-aria-props': 'warn',
+
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
-]);
+])
 
-export default eslintConfig;
+export default eslintConfig
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h2 className="bg-zinc-500 min-h-screen flex items-center justify-center text-white text-4xl font-bold uppercase">Hello, Next.js</h2>
+      <h2 className="flex min-h-screen items-center justify-center bg-zinc-500 text-4xl font-bold uppercase text-white">
+        Hello, Next.js
+      </h2>
     </div>
-  );
+  )
 }
