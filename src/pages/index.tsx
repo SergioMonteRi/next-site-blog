92c4ce72bc5a10b3161a5d163feb14fd1498eede
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-11-15 13:28:02 -0300

    feat: add Tailwind CSS configuration and UI components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from '@/components'
+
 export default function Home() {
   return (
-    <div>
-      <h2 className="flex min-h-screen items-center justify-center bg-zinc-500 text-4xl font-bold uppercase text-white">
+    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-zinc-500">
+      <h2 className="text-4xl font-bold uppercase text-white">
         Hello, Next.js
       </h2>
+
+      <Button variant="outline">Click me</Button>
     </div>
   )
 }
