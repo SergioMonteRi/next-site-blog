06d5f8acaa12ee16557214663e893d90d64ac741
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-11-20 07:33:21 -0300

    feat: integrate Contentlayer for markdown support; add initial Post document type and example blog post

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,21 @@
-import { Header } from '@/components'
-
 export default function Blog() {
+  // px-4 pb-20 pt-5 lg:px-48 lg:pb-32 lg:pt-12
+
   return (
-    <div className="min-h-screen bg-zinc-900">
-      <Header />
+    <div className="flex h-full flex-grow flex-col p-8 py-20 md:py-24">
+      <header>
+        <div className="container flex flex-col items-start justify-between space-y-6 px-4 md:flex-row md:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            <span className="w-fit rounded-md bg-cyan-300 px-8 py-2 text-center text-body-tag text-cyan-100 md:text-left">
+              Blog
+            </span>
+
+            <h1 className="max-w-2xl text-balance text-start text-heading-lg text-gray-100 md:text-left md:text-heading-xl">
+              Dicas e estratégias para impulsionar seu negócio
+            </h1>
+          </div>
+        </div>
+      </header>
     </div>
   )
 }
