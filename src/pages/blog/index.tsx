e2080daaa984c088aeb0fc06c546400a8abc9730
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-11-21 07:21:57 -0300

    feat: refactor blog page to use BlogList template; create BlogList component for improved layout and search functionality

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,23 +1,5 @@
-import { SearchInput } from '@/components'
+import { BlogList } from '@/templates'
 
 export default function Blog() {
-  return (
-    <div className="flex h-full flex-grow flex-col bg-gray-700 p-8 py-20 md:py-24">
-      <header>
-        <div className="container flex flex-col items-start justify-between space-y-6 md:flex-row md:items-end">
-          <div className="flex flex-col gap-4 md:px-0">
-            <span className="w-fit rounded-md bg-cyan-300 px-8 py-2 text-center text-body-tag text-cyan-100 md:text-left">
-              Blog
-            </span>
-
-            <h1 className="max-w-2xl text-balance text-start text-heading-lg text-gray-100 md:text-left md:text-heading-xl">
-              Dicas e estratégias para impulsionar seu negócio
-            </h1>
-          </div>
-
-          <SearchInput />
-        </div>
-      </header>
-    </div>
-  )
+  return <BlogList />
 }
---
+++ src/templates/blog/blog-list/index.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+
+import { SearchInput } from '@/components'
+
+export const BlogList = () => {
+  const router = useRouter()
+  const query = router.query.q as string
+  const pageTitle = query
+    ? `Resultados para "${query}"`
+    : 'Dicas e estratégias para impulsionar seu negócio'
+
+  return (
+    <div className="flex h-full flex-grow flex-col bg-gray-700 p-8 py-20 md:py-24">
+      <header>
+        <div className="container flex flex-col items-start justify-between space-y-6 md:flex-row md:items-end">
+          <div className="flex flex-col gap-4 md:px-0">
+            <span className="w-fit rounded-md bg-cyan-300 px-8 py-2 text-center text-body-tag text-cyan-100 md:text-left">
+              Blog
+            </span>
+
+            <h1 className="max-w-2xl text-balance text-start text-heading-lg text-gray-100 md:text-left md:text-heading-xl">
+              {pageTitle}
+            </h1>
+          </div>
+
+          <SearchInput />
+        </div>
+      </header>
+    </div>
+  )
+}
