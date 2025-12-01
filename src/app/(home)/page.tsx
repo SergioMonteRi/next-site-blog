20feb45baaea600eb5b002a26077b0f664fdd99c
SergioMonteRi <sergioribeiropalermo@gmail.com>
2025-12-01 18:45:59 -0300

    fix: update Open Graph metadata URLs for consistency and improved social sharing

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -9,13 +9,13 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Site.set',
     description: 'Venda seus produtos como afiliado em um único lugar',
-    url: 'https://next-site-blog-jvo5c439i-sergiomonteris-projects.vercel.app/',
+    url: 'https://next-site-blog.vercel.app/og-image.png',
     siteName: 'Site.set',
     locale: 'pt-BR',
     type: 'website',
     images: [
       {
-        url: 'https://next-site-blog-jvo5c439i-sergiomonteris-projects.vercel.app/og-image.png',
+        url: 'https://next-site-blog.vercel.app/og-image.png',
         width: 800,
         height: 600,
         alt: 'Site.set',
